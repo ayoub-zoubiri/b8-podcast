34d24134711aa89586bfdc22f110be01f158d341
Ayoub Zoubiri <ayoubzoubiriyc@gmail.com>
2025-11-10 15:08:10 +0100

    episode and podcast and users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nom' => fake()->lastName(),
+            'prenom' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement(['user','animateur']),
             'remember_token' => Str::random(10),
         ];
     }
